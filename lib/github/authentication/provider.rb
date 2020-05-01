bf1988cdf01e83349bfd9f8d55e82787499934db
Frederik Dudzik <frederik.dudzik@shopify.com>
2020-05-01 13:14:18 -0400

    init

---
+++ lib/github/authentication/provider.rb
@@ -0,0 +1,10 @@
+require "github/authentication/provider/version"
+
+module Github
+  module Authentication
+    module Provider
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/github/authentication/provider/version.rb
@@ -0,0 +1,7 @@
+module Github
+  module Authentication
+    module Provider
+      VERSION = "0.1.0"
+    end
+  end
+end
